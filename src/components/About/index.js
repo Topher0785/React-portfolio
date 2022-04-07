632ec45f36a168bbc91a8fe51eea37196d5f7b0d
christopher dale <csethdale@live.com>
2022-04-06 23:16:30 -0500

    updated with a Nav and indexJS in nav

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import coverImage from "../../assets/cover"
+import coverImage from "../../assets/cover/Chris, Reagy, Bubby.jpg";
 function About() {
     return(
 
         <section className="my-5">
             <h1 id = "about"> Who is Chirs D?</h1>
-            <img src={coverImage} className="my-2" style={{width: "100%"}} alt = "cover"/>
+            <img src={coverImage} className="my-2" style={{width: "10"}} alt = "cover"/>
         </section>
         
     );
